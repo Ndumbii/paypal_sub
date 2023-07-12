c810cefccc267b3ecaf1299c70d12b6dfba68b27
Ivy <116196352+Ndumbii@users.noreply.github.com>
2023-07-12 13:40:58 +0300

    Update accessToken.php

--- accessToken.php
+++ accessToken.php
@@ -6,7 +6,7 @@ curl_setopt($ch, CURLOPT_URL, 'https://api-m.sandbox.paypal.com/v1/oauth2/token'
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 curl_setopt($ch, CURLOPT_POST, 1);
 curl_setopt($ch, CURLOPT_POSTFIELDS, "grant_type=client_credentials");
-curl_setopt($ch, CURLOPT_USERPWD, 'AXbdG45D7opAtBbAbOj4dCOe2MXRoroD77wBEshpP2-VrwealpO8VQXmLRFb3jT4AuwXrDhthWQQOfCR' .':' . 'EPHdIZrOAfVZoCQqFOGca8YpL8cC0DQgIiS4e4-RmTlQWEWdkhLWGBLW3p5QPu_TTH3-hy9IGvV8OtNX');
+curl_setopt($ch, CURLOPT_USERPWD, 'CLIENTID' .':' . 'CLIENT SECRET');
 
 $headers = array();
 $headers[] = 'Accept: application/json';
